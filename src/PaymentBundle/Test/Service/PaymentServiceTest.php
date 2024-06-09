80b5a6d7104dbb6253d2010b347b90ff49158e84
Luiz Marques <l.fernando.9.lfsm@gmail.com>
2024-06-09 16:08:37 -0300

    subject studied: Exceptions testing

--- src/PaymentBundle/Test/Service/PaymentServiceTest.php
+++ src/PaymentBundle/Test/Service/PaymentServiceTest.php
@@ -5,6 +5,7 @@ namespace PaymentBundle\Test\Service;
 use OrderBundle\Entity\CreditCard;
 use OrderBundle\Entity\Customer;
 use OrderBundle\Entity\Item;
+use PaymentBundle\Exception\PaymentErrorException;
 use PaymentBundle\Repository\PaymentTransactionRepository;
 use PaymentBundle\Service\Gateway;
 use PaymentBundle\Service\PaymentService;
@@ -16,13 +17,38 @@ class PaymentServiceTest extends TestCase
     public function testShouldSaveWhenatewayReturnOkWithRetries(): void 
     {
         $Gateway = $this->createMock(Gateway::class);
-        $PaymentTransactionRepository = $this->createStub(PaymentTransactionRepository::class);
+        $PaymentTransactionRepository = $this->createMock(PaymentTransactionRepository::class);
         $PaymentService = new PaymentService($Gateway,$PaymentTransactionRepository);
 
         $Gateway->expects($this->atLeast(3))->method('pay')
             ->willReturn(
                 false,false,true
             );
+
+        $PaymentTransactionRepository->expects($this->once())->method('save');
+
+        $Customer = $this->createMock(Customer::class);
+        $Item = $this->createMock(Item::class);
+        $CreditCard = $this->createMock(CreditCard::class);
+
+        $PaymentService->pay($Customer,$Item,$CreditCard);
+
+    }    
+
+    public function testShouldThrowExceptionWhenGatewayFails(): void 
+    {
+        $Gateway = $this->createMock(Gateway::class);
+        $PaymentTransactionRepository = $this->createMock(PaymentTransactionRepository::class);
+        $PaymentService = new PaymentService($Gateway,$PaymentTransactionRepository);
+
+        $Gateway->expects($this->atLeast(3))->method('pay')
+            ->willReturn(
+                false,false,false
+            );
+
+        $this->expectException(PaymentErrorException::class);
+
+        $PaymentTransactionRepository->expects($this->never())->method('save');
 
         $Customer = $this->createMock(Customer::class);
         $Item = $this->createMock(Item::class);
